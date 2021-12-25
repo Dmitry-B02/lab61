56bb0a3ea7d9a7c1e25b0fdd4ed19625d87b7398
Dmitry-B02 <silaxer@mail.ru>
2021-12-25 14:59:10 +0300

    1

--- app/src/main/java/com/example/lab61/App.kt
+++ app/src/main/java/com/example/lab61/App.kt
@@ -6,10 +6,5 @@ import java.util.concurrent.Executors
 import java.util.concurrent.ExecutorService
 
 class App: Application() {
-    val executor: ExecutorService = Executors.newFixedThreadPool(1)
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.d("executor", "POOL WAS CREATED")
-    }
+    val executor: ExecutorService = Executors.newSingleThreadExecutor()
 }
